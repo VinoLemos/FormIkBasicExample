04a9001c86f4ff189f80d414ea1503533fe91bab
Vinícius Lemos de Oliveira <vlemosdeoliveira@gmail.com>
2024-08-31 19:55:53 -0300

    feat: basic form control with formik

--- src/components/AdicionaCliente.jsx
+++ src/components/AdicionaCliente.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
+import { Formik } from 'formik';
 
 const AdicionaCliente = () => {
   return (
     <>
       <h1>Cadastro de Clientes</h1>
 
-      <form>
-        <div className="form-group">
-          <label htmlFor="nome">Nome</label>
-          <input id="nome" name="nome" type="text" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input id="email" name="email" type="email" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="date">Data de Nascimento</label>
-          <input id="nascimento" name="nascimento" type="date" />
-        </div>
-        <button type="submit">Adicionar</button>
-      </form>
+      <Formik
+        initialValues={{
+          nome: '', // Valor padrão dos campos (chamar props.values.nome)
+          email: '',
+          nascimento: ''
+        }}
+        onSubmit={(values) => {
+          alert(JSON.stringify(values));
+        }}
+      >
+        {props => (
+          <form noValidate onSubmit={props.handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="nome">Nome</label>
+              <input id="nome" name="nome" type="text" value={props.values.nome} onChange={props.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input id="email" name="email" type="email" value={props.values.email} onChange={props.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="date">Data de Nascimento</label>
+              <input id="nascimento" name="nascimento" type="date" value={props.values.nascimento} onChange={props.handleChange} />
+            </div>
+            <button type="submit">Adicionar</button>
+          </form>
+        )}
+      </Formik>
     </>
   );
 };
